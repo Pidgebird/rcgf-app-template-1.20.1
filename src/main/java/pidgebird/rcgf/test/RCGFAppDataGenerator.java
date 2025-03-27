3cf661a20e1602d51f0c6942da26a18f46b7db8c
spiderskramz <spiderskramz@gmail.com>
2025-03-27 08:44:51 -0700

    yayyyy works

---
+++ src/main/java/pidgebird/rcgf/test/RCGFAppDataGenerator.java
@@ -0,0 +1,11 @@
+package pidgebird.rcgf.test;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RCGFAppDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
